e3ff1fb4733ecccdc34dced8e37f0a3631fd2f1a
Purna-ASU <ppeddire@asu.edu>
2023-08-07 14:44:10 -0700

    About section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
+import logo from "./logo.svg";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Fun-Facts</h1>
+      <img src={logo} width="90px" alt="logo" />
+      <h1>Fun Facts about React</h1>
       <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>Was released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on Github</li>
+        <li>is developed by Facebook</li>
+        <li>Powers thousands of enterprise apps, including moblie apps</li>
       </ul>
     </div>
   );
